027a5d42cf8c438af648749a8612e6e757de1c30
Yevhen <tunkin.yevhen@lll.kpi.ua>
2023-06-21 10:23:46 +0300

    moved connectionstring to appsettings

--- Models/ChatDbContext.cs
+++ Models/ChatDbContext.cs
@@ -21,8 +21,13 @@ public partial class ChatDbContext : DbContext
 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:resources.database.windows.net,1433;Initial Catalog=ChatDB;Persist Security Info=False;User ID=manager;Password=Kaktus456;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("ChatDb"));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
